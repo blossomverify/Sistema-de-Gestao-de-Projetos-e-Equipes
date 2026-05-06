60795ee751e60fb208a9bfcf8b7b7aa8ffa6361e
Seu Nome <mariceliasouzaalves@gmail.com>
2026-05-05 22:14:37 -0300

    Atualizacao

--- database/model/Usuario.java
+++ database/model/Usuario.java
@@ -1,5 +1,17 @@
 package database.model;
 
 public class Usuario {
-    
+    private String nome, login, senha, perfil;
+
+    public Usuario(String nome, String login, String senha, String perfil) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
+
+    public String getNome() { return nome; }
+    public String getLogin() { return login; }
+    public String getSenha() { return senha; }
+    public String getPerfil() { return perfil; }
 }
