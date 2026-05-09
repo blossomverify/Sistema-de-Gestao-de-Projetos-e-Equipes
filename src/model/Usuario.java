4f4ab42d3861502c700d7f5cb4ac1273080c6e50
Murilo <15041329630@ulife.com.br>
2026-05-09 03:47:53 -0300

    Usuario.java adicionar get

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -9,4 +9,55 @@ public class Usuario {
     private String login;
     private String perfil;
     
+
+
+    public string getNome() {
+        return this.nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public string getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(string senha) {
+        this.senha = senha;
+    }
+
+    public string getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(string email) {
+        this.email = email;
+    }
+
+    public string getCargo() {
+        return this.cargo;
+    }
+
+    public void setCargo(string cargo) {
+        this.cargo = cargo;
+    }
+
+    public string getLogin() {
+        return this.login;
+    }
+
+    public void setLogin(string login) {
+        this.login = login;
+    }
+
+    public string getPerfil() {
+        return this.perfil;
+    }
+
+    public void setPerfil(string perfil) {
+        this.perfil = perfil;
+    }
+
+
 }
