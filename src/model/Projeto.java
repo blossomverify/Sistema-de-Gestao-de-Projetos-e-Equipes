b26a59a41d85da1ec6848c7665c3da6c7a63583e
Murilo <15041329630@ulife.com.br>
2026-05-09 00:06:45 -0300

    Fiz projeto.java e usuario.java

--- src/model/Projeto.java
+++ src/model/Projeto.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Projeto {
-    
+    String nome;
+    String descricao;
+    String dataInicio;
+    String dataTermino;
+    String status;
+    String gerente;
+
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Usuario {
+public class Usuario { 
+
+
+
+    String nome;
+    String senha;
+    String email;
+    String cargo;
+    String login;
+    String perfil;
     
 }
