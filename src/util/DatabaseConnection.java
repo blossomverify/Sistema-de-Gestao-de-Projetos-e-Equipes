2b9f3ad6b3699f087bcf8576e3eefdd30f8166ad
Murilo <15041329630@ulife.com.br>
2026-05-11 23:36:37 -0300

    Funcionalidade: adicionar mensagem de erro amigavel para falha na conexao com o banco de dados

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -3,6 +3,7 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/projeto_a3";
@@ -13,6 +14,7 @@ public class DatabaseConnection {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar ao MySQL!\n1. Verifique se o MySQL está ligado.\n2. Verifique se o banco 'projeto_a3' foi criado.", "Erro de Conexão", JOptionPane.ERROR_MESSAGE);
             throw new RuntimeException("Erro ao conectar ao banco de dados", e);
         }
     }
